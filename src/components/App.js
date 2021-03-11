cbbf3a13f8395b0e5373e39e040f9e10b4af8efb
v1997 <patelvasu1997@gmail.com>
2021-03-11 23:21:13 +0530

    para id and button added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import Welcome from "./Welcome";
 
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			para: ''
+		}
+		this.handleClick = this.handleClick.bind(this);
+
 	};
 
+	handleClick() {
+		this.state.para === '' ? this.setState({para: 'Hello, I\'ve learnt to use the full-stack evaluation tool. This makes me so happy'}) : this.setState({para: ''});
+	}
+
     render() {
+		const { para } = this.state;
+		// para === '' ? para: '';
     	return(
     		<div id="main">
-				<Welcome greeting="vasu"/>
+				<p id="para">{para}</p>
+				<button onClick={this.handleClick} id="click">click</button>
 				{ /* Do not remove this main div!! */ }
     		</div>
     	);
--- src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-const Welcome = ({greeting}) => {
-    return <>
-    <h1>Hey !{greeting}</h1>
-    <h2>Welcome to Newton School.</h2>
-    </>
-}
-export default Welcome
